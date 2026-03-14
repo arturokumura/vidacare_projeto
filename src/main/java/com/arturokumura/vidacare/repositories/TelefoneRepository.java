4c8b1ea509258243ff28e9e396d43be64d4dd2f1
arturokumura <arturokumura2008@gmail.com>
2026-03-14 00:01:23 -0300

    Telefone Repository

---
+++ src/main/java/com/arturokumura/vidacare/repositories/TelefoneRepository.java
@@ -0,0 +1,8 @@
+package com.arturokumura.vidacare.repositories;
+
+import com.arturokumura.vidacare.model.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelefoneRepository extends JpaRepository <Telefone , Long > {
+
+}
