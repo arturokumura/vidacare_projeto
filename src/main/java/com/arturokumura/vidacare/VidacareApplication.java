fb08ed4128ceb4723c8b9fdb1bf54d669d29fd2b
arturokumura <arturokumura2008@gmail.com>
2026-03-13 23:32:41 -0300

    Criei projeto

---
+++ src/main/java/com/arturokumura/vidacare/VidacareApplication.java
@@ -0,0 +1,13 @@
+package com.arturokumura.vidacare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidacareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidacareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arturokumura/vidacare/VidacareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arturokumura.vidacare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidacareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
