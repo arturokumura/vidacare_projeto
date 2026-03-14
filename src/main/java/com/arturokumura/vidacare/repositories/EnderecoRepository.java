cb38b24a9110d32115a27ade417cfa2aaa6f00da
arturokumura <arturokumura2008@gmail.com>
2026-03-14 00:02:40 -0300

    Endereco Repository

---
+++ src/main/java/com/arturokumura/vidacare/repositories/EnderecoRepository.java
@@ -0,0 +1,8 @@
+package com.arturokumura.vidacare.repositories;
+
+import com.arturokumura.vidacare.model.Endereco;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnderecoRepository extends JpaRepository <Endereco, Long > {
+
+}
