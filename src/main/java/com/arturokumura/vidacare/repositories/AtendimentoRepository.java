a43f606a219829f57d62688551f1771017c6361e
arturokumura <arturokumura2008@gmail.com>
2026-03-14 00:03:44 -0300

    Atendimento Repository

---
+++ src/main/java/com/arturokumura/vidacare/repositories/AtendimentoRepository.java
@@ -0,0 +1,8 @@
+package com.arturokumura.vidacare.repositories;
+
+import com.arturokumura.vidacare.model.Atendimento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AtendimentoRepository extends JpaRepository <Atendimento, Long > {
+
+}
