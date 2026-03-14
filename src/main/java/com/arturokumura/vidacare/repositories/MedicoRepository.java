99399f4bed4fa80d80bac6de53500faa41859ac5
arturokumura <arturokumura2008@gmail.com>
2026-03-13 23:57:40 -0300

    Medico Repository

---
+++ src/main/java/com/arturokumura/vidacare/repositories/MedicoRepository.java
@@ -0,0 +1,8 @@
+package com.arturokumura.vidacare.repositories;
+
+import com.arturokumura.vidacare.model.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository< Medico, Long > {
+
+}
